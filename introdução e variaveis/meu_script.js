971a7c36c8702186acd31a5fe555c56c9ff6b037
Leonardo Andrade <renraksVEVO@hotmail.com>
2021-03-17 20:05:50 -0300

    primeira adição

---
+++ introdução e variaveis/meu_script.js
@@ -0,0 +1,17 @@
+//Comentários são feitos com // ou /**/ 
+
+//alert('Bem vindo ao curso, por arquivo externo dessa vez !!!')
+
+/*
+    selecionar um elemento do DOM
+    atualizar o valor desse elemento
+*/
+
+
+document.getElementById('nome').value = 'Oi'
+
+var a = 10
+
+var b = 20
+
+var c = null
